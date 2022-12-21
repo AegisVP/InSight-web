417f3d4d726c8997aa425912b48e54fdb1f88986
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-12-21 19:51:33 +0200

    commit

--- src/redux/diet/dietOperations.js
+++ src/redux/diet/dietOperations.js
@@ -5,7 +5,7 @@ export const getDiet = createAsyncThunk(
   '/diet',
   async ({ height, age, current_weight, desired_weight, blood_type }, thunkAPI) => {
     try {
-      const response = await axios.get('/diet', { height, age, current_weight, desired_weight, blood_type });
+      const response = await axios.post('/diet', { height, age, current_weight, desired_weight, blood_type });
       return response.data;
     } catch (err) {
       return thunkAPI.rejectWithValue(err.message);
